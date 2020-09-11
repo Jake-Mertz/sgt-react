52e9a2acf3172af8a71e6c845912bb9ef08e419b
Jake-Mertz <jakemertz101@gmail.com>
2020-09-11 12:19:41 -0400

    view-all-grades

--- client/components/GradeTable.jsx
+++ client/components/GradeTable.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import Grade from './Grade';
+// import React from 'react';
+// import Grade from './Grade';
 // import App from './app';
 
-function GradeTable(props) {
-  // console.log(<Grade/>);
-  // console.log(props);
-  return <table className="gradeTable"><Grade myData={props.grades}/>
-    <thead></thead>
-    <tbody>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
+// function GradeTable(props) {
+//   // console.log(<Grade/>);
+//   console.log(props);
+//   return (
+//         <table className="gradeTable"><Grade myData={props.grades}/>
+//             props.grades.map(myGrades => {
+//       return (
+//           <th></th>
+//           <tbody>
+//             <tr>
+//               <td key={myGrades.name}></td>
+//               <td key={myGrades.course}></td>
+//               <td key={myGrades.grade}></td>
+//             </tr>
+//           </tbody>
+//           </table>
+//             );
+//           )
+//         }
+//       }
+//       }
 
-  </table>;
-}
+// function GradeTable(props) {
+//   return (
+//     <table className="gradeTable">
+//       {
+//         props.grades.map(theGrades => {
+//         // return (
+//         // <th></th>
+//         return (
+//         <Grade
+//           <tbody>
+//             <tr>
+//               <td key={props.grades.name}></td>
+//               <td key={props.grades.course}></td>
+//               <td key={props.grades.grade}></td>
+//             </tr>
+//           </tbody>
+//           />
+//         // );
+//         })
+//       }
+//     </table>
+//   );
+//   )
+// }
 
-export default GradeTable;
+// export default GradeTable;
--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -1,16 +1,15 @@
-// import React from 'react';
+import React from 'react';
 // import GradeTable from './GradeTable';
 // import App from './app';
 
 function Grade(props) {
   // console.log(props);
   return (
-    // <tr>
-    //   <td>{props.grades.name}</td>
-    //   <td>{props.grades.course}</td>
-    //   <td>{props.grades.grade}</td>
-    // </tr>
-    null
+    <tr>
+      <td>{props.grades.name}</td>
+      <td>{props.grades.course}</td>
+      <td>{props.grades.grade}</td>
+    </tr>
   );
 }
 
