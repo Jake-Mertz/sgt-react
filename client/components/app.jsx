95bc04f80c4c105864fa056c1fdbd7f58aedb023
Jake-Mertz <jakemertz101@gmail.com>
2020-09-13 15:58:42 -0400

    view-all-grades

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -13,6 +13,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.getGrades();
+    this.getAverageGrade();
   }
 
   getGrades() {
@@ -23,20 +24,41 @@ class App extends React.Component {
       });
   }
 
-  getAverageGrade() {
+  // getAverageGrade() {
+  //   let sum;
+  //   fetch('/api/grades')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       for (let i = 0; i < data.length; i++) {
+  //         sum += data[i].grade;
+  //       }
+  //       const average = sum / data.length;
+  //       const averageRounded = Math.floor(average);
+  //       return averageRounded;
+  //     });
+  // }
 
+  getAverageGrade(props) {
+    // console.log(this.props.state.grades);
+    let sum;
+    for (let i = 0; i < this.grades.length; i++) {
+      sum += this.state.grades[i].grade;
+    }
+    const average = sum / this.grades.length;
+    const averageRounded = Math.floor(average);
+    return averageRounded;
   }
 
   render() {
-
     const { grades } = this.state;
+    // console.log(this.averageRounded);
     // const grades = this.state.grades;
-
+    // console.log(grades);
     // console.log(this.props);
     // console.log(this.state);
     return (
       <div className="container">
-        <Header />
+        <Header average={this.averageRounded}/>
         <GradeTable grades={grades} />
       </div>
     );
